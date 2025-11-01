9092e1901d13be00d560bfa88358bd3a1d0b8c01
ninefish <593378570@qq.com>
2025-11-01 10:32:01 +0800

    ok

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,8 +10,11 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
+        while (true) {
             head.first += c;
+            if(head.rest == null){
+                break;
+            }
             head = head.rest;
         }
     }
@@ -51,7 +54,7 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while(x >= 10){
             x = x / 10;
         }
         int firstDigit = x % 10;
@@ -70,6 +73,7 @@ public class IntListExercises {
         if (lst == null) {
             return false;
         }
+        boolean restHasPrime = squarePrimes(lst.rest);
 
         boolean currElemIsPrime = Primes.isPrime(lst.first);
 
@@ -77,6 +81,6 @@ public class IntListExercises {
             lst.first *= lst.first;
         }
 
-        return currElemIsPrime || squarePrimes(lst.rest);
+        return currElemIsPrime || restHasPrime;
     }
 }
