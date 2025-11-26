a82f4f1bbe7342c2f34cd9c311031c007935ef83
ninefish <593378570@qq.com>
2025-11-26 22:08:53 +0800

    s

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,102 +1,153 @@
 package deque;
 
-public class ArrayDeque<T> implements Deque<T>{
-    private T[] Deque ;
+import java.util.Iterator;
+
+public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
+    private int length;
     private int size;
-    private int nextFirst;
-    private int nextLast;
+    private int first;
+    private int last;
+    private T[] arr;
 
-    public ArrayDeque(){
-        Deque = (T[]) new Object[8];
-        size = 0;
-        nextFirst = 3;
-        nextLast = 4;
+    public ArrayDeque() {
+        this.arr = (T[]) new Object[8];
+        this.size = 8;
+        this.length = 0;
+        this.first = 0;
+        this.last = 0;
     }
 
-    public void resize(int Dequesize){
-        T[] newDeque = (T[]) new Object[Dequesize];
-
-        for (int i = 0; i < size; i++) {
-            int oldIndex = ((nextFirst + 1) % Deque.length + i) % Deque.length;
-            newDeque[i] = Deque[oldIndex];
+    @Override
+    public void addFirst(T item) {
+        if (this.length == this.size - 1) {
+            reSize(this.size * 2);
         }
-
-        Deque = newDeque;
-        nextFirst = Dequesize - 1;
-        nextLast = size;
-    }
-
-    @Override
-    public void addFirst(T item){
-        if(size == Deque.length){
-            resize(Deque.length * 2);
+        if (!isEmpty()) {
+            this.first = (this.first - 1 + this.size) % this.size;
         }
-        Deque[nextFirst] = item;
-        nextFirst = (nextFirst - 1 + Deque.length) % Deque.length;
-        size++;
+        this.arr[first] = item;
+        this.length++;
     }
 
     @Override
-    public void addLast(T item){
-        if(size == Deque.length){
-            resize(Deque.length * 2);
+    public void addLast(T item) {
+        if (this.length == this.size - 1) {
+            reSize(this.size * 2);
         }
-        Deque[nextLast] = item;
-        nextLast = (nextLast + 1) % Deque.length;
-        size++;
-    }
-
-    @Override
-    public int size(){
-        return size;
-    }
-
-    @Override
-    public T get(int index){
-        if (index < 0 || index >= size) {
-            return null;
+        if (!isEmpty()) {
+            this.last = (this.last + 1) % this.size;
         }
-        int realindex = ((nextFirst + 1) % Deque.length + index) % Deque.length;
-        return Deque[realindex];
+        this.arr[this.last] = item;
+        this.length++;
     }
 
     @Override
-    public void printDeque(){
-        for(int i = 0;i < size;i++){
-            System.out.print(get(i) + " ");
+    public int size() {
+        return this.length;
+    }
+
+    @Override
+    public void printDeque() {
+        int i = this.first;
+        while (i != this.last) {
+            System.out.print(this.arr[i] + " ");
+            i = (i + 1) % this.size;
         }
-        System.out.println();
+        System.out.println(this.arr[i]);
     }
 
     @Override
-    public T removeFirst(){
+    public T removeFirst() {
         if (isEmpty()) {
             return null;
         }
-        int firstIndex = (nextFirst + 1) % Deque.length;
-        T item = Deque[firstIndex];
-        Deque[firstIndex] = null;
-        nextFirst = firstIndex;
-        size--;
-        if(size < Deque.length / 4){
-            resize(Deque.length / 2);
+        this.length--;
+        T ret = this.arr[this.first];
+        this.first = (this.first + 1) % this.size;
+        if (this.length * 4 < this.size) {
+            reSize(this.size / 2);
         }
-        return item;
+        return ret;
     }
 
     @Override
-    public T removeLast(){
+    public T removeLast() {
         if (isEmpty()) {
             return null;
         }
-        int LastIndex = (nextLast - 1 + Deque.length) % Deque.length;
-        T item = Deque[LastIndex];
-        Deque[LastIndex] = null;
-        nextLast = LastIndex;
-        size--;
-        if(size < Deque.length / 4){
-            resize(Deque.length / 2);
+        this.length--;
+        T ret = this.arr[this.last];
+        this.last = (this.last - 1 + this.size) % this.size;
+        if (this.length * 4 < this.size) {
+            reSize(this.size / 2);
         }
-        return item;
+        return ret;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= this.length) {
+            return null;
+        }
+        int position = (index + this.first) % this.size;
+        return this.arr[position];
+    }
+
+    private void reSize(int s) {
+        T[] temp = (T[]) new Object[s];
+        int i = this.first;
+        int j = 0;
+        while (j < this.length) {
+            temp[j] = this.arr[i];
+            i = (i + 1) % this.size;
+            j++;
+        }
+        this.size = s;
+        this.first = 0;
+        if (isEmpty()) {
+            this.last = 0;
+        } else {
+            this.last = j - 1;
+        }
+        this.arr = temp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Deque) || ((Deque<?>) o).size() != this.size()) {
+            return false;
+        }
+        if (o == this) {
+            return true;
+        }
+        for (int i = 0; i < this.size(); i++) {
+            Object item = ((Deque<?>) o).get(i);
+            if (!(this.get(i).equals(item))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Iterator<T> iterator() {
+        return new Iterable();
+    }
+
+    private class Iterable implements Iterator<T> {
+        private int pos;
+
+        Iterable() {
+            this.pos = 0;
+        }
+
+        public boolean hasNext() {
+            return this.pos < length;
+        }
+
+        public T next() {
+            T ret = get(this.pos);
+            this.pos++;
+            return ret;
+        }
     }
 }
